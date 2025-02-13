fa95a6a387cad6ac8d91c3bd8cc4832318faf126
HakonK-JobLoop <haakon.k@jobloop.no>
2025-02-13 22:05:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div className="body">
+      <div className="mainContainer">
+        <h1 className="herName">Johanna</h1>
+        <div className="fadingLine"></div>
+        <p className="loveMessage">
+          Jeg elsker deg <br />
+          Hver dag med deg er spesiell💕
+        </p>
+      </div>
+    </div>
+  );
+}
